78ab3b1882778b7ea6f95f92e63ec91d3b677219
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2024-10-02 12:12:34 +0530

    bubble

--- Sorting/bubble.java
+++ Sorting/bubble.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 public class bubble {
     public static void buublesort(int a[],int size)
     {  int temp;
-        for(int i=0;i<size;i++)  // loop for number of rounds
+        // for(int i=0;i<size;i++)  // loop for number of rounds
+        // {
+        //     for(int j=0;j<size-1-i;j++) // loop to check for adjacent elements
+        //     {
+        //         if(a[j]<a[j+1])
+        //         {
+        //             temp=a[j];
+        //             a[j]=a[j+1];
+        //             a[j+1]=temp;
+        //         }
+        //     }
+        // }
+        for(int i=0;i<size;i++)
         {
-            for(int j=0;j<size-1-i;j++) // loop to check for adjacent elements
+            for(int j=0;j<size-1-i;j++)
             {
-                if(a[j]<a[j+1])
+                if(a[j]>a[j+1])
                 {
                     temp=a[j];
                     a[j]=a[j+1];
