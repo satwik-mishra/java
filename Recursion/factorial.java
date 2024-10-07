70ad7ab6bb5fa7ff9ca335b88b6023dc97bb2029
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2024-10-07 20:04:47 +0530

    factorial

---
+++ Recursion/factorial.java
@@ -0,0 +1,21 @@
+package Recursion;
+import java.util.*;
+public class factorial {
+    public static int fact(int n)
+    {
+       if(n==0)
+       {
+        return 1;
+       }
+       return n*fact(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=ob.nextInt();
+        System.out.println("factorial of the number is"+ fact(n));
+   
+   ob.close();
+    }
+}
