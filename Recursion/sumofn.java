a14bd8d7afb2cb7c3c716c82d037f9587682fa19
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2024-10-08 16:25:30 +0530

    sum of n

---
+++ Recursion/sumofn.java
@@ -0,0 +1,22 @@
+package Recursion;
+import java.util.*;
+public class sumofn {
+    public static int sum(int n)
+    {
+        if(n==1)
+        {
+            return n;
+        }
+        int smn1=sum(n-1);
+        int sn=n+smn1;
+        return sn;
+
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=ob.nextInt();
+        System.out.println("the sum of n numbers = "+sum(n));
+        ob.close();
+    }
+}
